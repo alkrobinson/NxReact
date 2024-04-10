fff37e2c4a17f62957184e6ea40de1ede406a2af
Alisha Robinson <alkgreer@gmail.com>
2024-04-10 22:37:54 +0000

    move files and make grid

---
+++ src/app/app.tsx
@@ -0,0 +1,20 @@
+import './app.scss';
+
+export default function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <div className="box">
+        <div className="inner-box-first-row"></div>
+        <div className="inner-box-first-row"></div>
+        <div className="inner-box-first-row"></div>
+        <div className="inner-box-second-row"></div>
+        <div className="inner-box-second-row"></div>
+        <div className="inner-box-second-row"></div>
+        <div className="inner-box-third-row"></div>
+        <div className="inner-box-third-row"></div>
+        <div className="inner-box-third-row"></div>
+      </div>
+    </>
+  );
+}

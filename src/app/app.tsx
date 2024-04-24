ed2d64f88d418030694bb31afbfea4eeb8c267c7
Alisha Robinson <alkgreer@gmail.com>
2024-04-24 15:57:32 +0000

    simplify styles

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,16 +5,26 @@ export default function App() {
     <>
       <h1>App</h1>
       <div className="box">
-        <div className="inner-box-first-row"></div>
-        <div className="inner-box-first-row"></div>
-        <div className="inner-box-first-row"></div>
-        <div className="inner-box-second-row"></div>
-        <div className="inner-box-second-row"></div>
-        <div className="inner-box-second-row"></div>
-        <div className="inner-box-third-row"></div>
-        <div className="inner-box-third-row"></div>
-        <div className="inner-box-third-row"></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
+        <div className="inner-box" onClick={addX}></div>
       </div>
     </>
   );
+}
+
+function addX(event: any) {
+  const x = document.createElement('div');
+  x.style.display = 'flex';
+  x.style.justifyContent = 'center';
+  x.style.alignItems = 'center';
+  x.style.height = '100%';
+  x.textContent = 'X';
+  event.target.appendChild(x);
 }
